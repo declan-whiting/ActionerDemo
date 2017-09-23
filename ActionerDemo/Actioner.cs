78734cec1bc60965c6e9338b90a0dfae1f25c714
declan-whiting <declan-whiting@outlook.com>
2017-09-23 13:55:14 +0100

    Do action method fixed

--- ActionerDemo/Actioner.cs
+++ ActionerDemo/Actioner.cs
@@ -6,7 +6,7 @@ namespace ActionerDemo
     public class Actioner
     {
         private static readonly SemaphoreSlim _semaphoreSlim = new SemaphoreSlim(1,1);
- 
+        
         public async void DoAction(Task task)
         {
             await _semaphoreSlim.WaitAsync();
