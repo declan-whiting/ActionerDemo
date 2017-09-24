b12026049e9e5bb649783088959913bf8f10c707
declan-whiting <declan-whiting@outlook.com>
2017-09-24 22:12:42 +0100

    comments corrected

--- ActionerDemo/Actioner.cs
+++ ActionerDemo/Actioner.cs
@@ -9,18 +9,18 @@ namespace ActionerDemo
         //Only one thread at a time can access this 
         private static readonly SemaphoreSlim _semaphoreSlim = new SemaphoreSlim(1,1);
         
+        //Async method does not block any other thread 
         public async void DoAction(Task task)
         {
-            //Asynchronously wait to enter the Semaphore.
-            //Thread will queue here until the sempahore's count is 0 
+            //Asynchronously queue here until the sempahore's count is 0 
             await _semaphoreSlim.WaitAsync();
 
             try //Count = 1
             {
-               //Runs the task synchronously to ensure tasks run one at a time
+               //Awaits the task synchronously to ensure tasks run one at a time
                await Task.Run(() => task.RunSynchronously()); 
             }
-            finally //Ensure release in finally clause
+            finally //Ensure release using finally clause
             {
                 _semaphoreSlim.Release(); //Count = 0
             }
